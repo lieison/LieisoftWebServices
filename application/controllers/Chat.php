06635ea955e21df86b167e6576d23991019699d7
Rolando Antonio Arriaza <rolignu90@gmail.com>
2015-06-11 19:56:34 -0600

    merger

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -9,13 +9,15 @@ class Chat  extends CI_Controller{
     }
     
     public function index(){
-        $this->output->set_header("");
+        //header('Access-Control-Allow-Origin: *');
+        $this->output->set_header("Access-Control-Allow-Origin: *");
         echo "(" . json_encode($this->chat_model->getchat()) . ");";
     }
     
     public function add($id , $message){
+         $this->output->set_header("Access-Control-Allow-Origin: *");
          $request = $this->chat_model->setchat($id, $message);
-         
+         echo $request;
     }
     
    
